84ab1d52c3536df8f5d63c8c7e709ddb8c9e420f
Leonardo Rodrigues Marques <leorm037@gmail.com>
2020-11-16 20:30:15 -0300

    Limitar o resultado das consultas

--- json.php
+++ json.php
@@ -4,7 +4,7 @@ require_once('db.php');
 
 header('Content-Type: application/json');
 
-$sql = "SELECT CONVERT_TZ(timestamp,'+00:00','-03:00') as timestamp, download, upload FROM speedtest ORDER BY timestamp ASC";
+$sql = "SELECT CONVERT_TZ(timestamp,'+00:00','-03:00') as timestamp, download, upload FROM speedtest ORDER BY timestamp DESC LIMIT 40";
 
 $stmt = $pdo->query($sql);
 
@@ -18,6 +18,6 @@ foreach($velocidades as $velocidade){
 	$data[] = array($timestamp,$download,$upload);
 }
 
-echo json_encode($data);
+echo json_encode(array_reverse($data));
 
 $pdo = null;
