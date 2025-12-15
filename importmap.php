64fef5c579ed8a66ed83695b5185d59b033b8839
Leonardo Rodrigues Marques <leorm037@gmail.com>
2025-12-15 20:03:21 -0300

    Correção .gitignore

--- importmap.php
+++ importmap.php
@@ -9,18 +9,4 @@
  *     with this source code in the file LICENSE.
  */
 
-return [
-    'app' => [
-        'path' => './assets/app.js',
-        'entrypoint' => true,
-    ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
-];
+return [];
